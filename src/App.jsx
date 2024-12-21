727603fef6c2ab97fa43e6e75d8cdb66fd9320b6
Mustafe-Abdirahman <khadar3021@gmail.com>
2024-12-21 13:38:02 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./assets/pages/About";
-import Home from "./assets/pages/Home";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Header />
     </BrowserRouter>
   );
 }
